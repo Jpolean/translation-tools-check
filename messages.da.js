59e607ec41ff5ab15e67a365a9c875c8c2535fa6
Jpolean <jpolean@pm.me>
2024-10-30 16:08:12 +0100

    Create messages.da.js

---
+++ messages.da.js
@@ -0,0 +1,9 @@
+import { messages as VisitationForm } from "../components/form/VisitationForm";
+
+const translations = {
+	[ProfessionalAssignee.label_VUM_visitation.id]: "Ansvarlig for udredning",
+	[ProfessionalAssignee.label_VUM_problem.id]: "Ansvarlig for indsatsm\u00E5l",
+	[VisitationClassificationMessages.noDataAvailable.id]: "Ingen data tilg\u00E6ngelig"
+};
+
+export default translations;
