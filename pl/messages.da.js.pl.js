18ec3c21c0841d347089d78b90b057155b5b66fb
Jpolean <jpolean@pm.me>
2024-10-30 16:13:08 +0100

    New translations messages.da.js (Polish)

---
+++ pl/messages.da.js.pl.js
@@ -0,0 +1,7 @@
+import { messages as VisitationForm } from '../components/form/VisitationForm';
+const translations = {
+    [ProfessionalAssignee.label_VUM_visitation.id]: 'Ansvarlig for udredning',
+    [ProfessionalAssignee.label_VUM_problem.id]: 'Ansvarlig for indsatsmål',
+    [VisitationClassificationMessages.noDataAvailable.id]: 'Ingen data tilgængelig',
+};
+export default translations;
