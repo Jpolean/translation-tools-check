e44cc672f582b7ccf154be72cf1ebf34f355b88a
Jacek Popiel <yop@kmd.dk>
2024-11-01 11:21:09 +0100

    commit1

--- messages.da.js
+++ messages.da.js
@@ -1,5 +1,3 @@
-import { messages as VisitationForm } from "../components/form/VisitationForm";
-
 const translations = {
 	[ProfessionalAssignee.label_VUM_visitation.id]: "Ansvarlig for udredning",
 	[ProfessionalAssignee.label_VUM_problem.id]: "Ansvarlig for indsatsm\u00E5l",
