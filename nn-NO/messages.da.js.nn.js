1c07df1b5a1cb69e262a1f995c89b28660c56ae5
Jpolean <jpolean@pm.me>
2024-11-04 15:59:56 +0100

    New translations messages.da.js (Norwegian Nynorsk)

---
+++ nn-NO/messages.da.js.nn.js
@@ -0,0 +1,6 @@
+const translations = {
+    [ProfessionalAssignee.label_VUM_visitation.id]: 'Ansvarlig for udredning',
+    [ProfessionalAssignee.label_VUM_problem.id]: 'Ansvarlig for indsatsmål',
+    [VisitationClassificationMessages.noDataAvailable.id]: 'Ingen data tilgængelig',
+};
+export default translations;
