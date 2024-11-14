70acccc77921f1167d7e2d03fb2f28fc18bac0ea
Jacek Popiel <yop@kmd.dk>
2024-11-14 09:44:18 +0100

    commit4

--- messages.da.js
+++
@@ -1,7 +0,0 @@
-const translations = {
-	[ProfessionalAssignee.label_VUM_visitation.id]: "Ansvarlig for udredning",
-	[ProfessionalAssignee.label_VUM_problem.id]: "Ansvarlig for indsatsm\u00E5l",
-	[VisitationClassificationMessages.noDataAvailable.id]: "Ingen data tilg\u00E6ngelig"
-};
-
-export default translations;
